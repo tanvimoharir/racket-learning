8b8108f5e648d418a5a55101b5aba11bbb14964a
Tanvi Moharir <tanvimoharir@gmail.com>
2023-03-08 17:11:18 +0530

    laws and recursions

--- littleSchemer/first.rkt
+++ littleSchemer/first.rkt
@@ -8,3 +8,16 @@
   (lambda (x)
     (or (atom? x) (list? x))))
 
+(define lat?
+  (lambda (l)
+    (cond
+      [(null? l) #t]
+      [(atom? (car l)) (lat? (cdr l))]
+      [else #f])))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      [(null? lat) #f]
+      [else (or (eq? (car lat) a)
+                (member? a (cdr lat)))])))
